1f63da41aa5031a7390fcf496a05dd1b46aeeccb
mirthful-concept-6030 <jchristinem@gmail.com>
2020-07-25 17:28:51 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,11 @@
 class Shoe
-  #attr_accessor :color, :size, :material, :condition
-  #attr_reader :brand
-  #def initialize(brand)
-  #  @brand=brand
-#  end
-  #def cobble
-  #  @condition="new"
-    #puts "The shoe has been repaired"
-#  end
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+  def initialize(brand)
+    @brand=brand
+  end
+  def cobble
+   @condition="new"
+    puts "The shoe has been repaired"
+ end
 end
